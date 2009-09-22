676a4036b17fb683b70601309027a6b9c9f9eb07
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2009-09-22 10:31:13 -0400

    default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => "verbs"
   map.resources :verbs
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
 
