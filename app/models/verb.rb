83060735aea22faa33dcf73cdf40e90fd1e2626e
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2009-09-22 11:28:30 -0400

    use websolr for full text search

--- app/models/verb.rb
+++ app/models/verb.rb
@@ -1,8 +1,10 @@
 class Verb < ActiveRecord::Base
   is_taggable
+  acts_as_solr
   
   validates_presence_of :command
   after_validation :extract_name
+  
   
   def extract_name
     self.name = self.command.to_s.match(/\S+/)[0]
