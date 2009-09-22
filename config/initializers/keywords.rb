d28b194f6d42d79d6b05274a1442d030b9857af5
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2009-09-22 15:11:48 -0400

    Putting navigation together

--- config/initializers/keywords.rb
+++ config/initializers/keywords.rb
@@ -12,3 +12,30 @@ Keywords = %w{
   self
   instrument
 }
+
+Races = [
+  "Dwarv",
+  "Elf",
+  "Elothean",
+  "Gor'Tog",
+  "Gnome",
+  "Halfling",
+  "Human",
+  "Kaldar",
+  "Prydaen",
+  "S'Kra Mur",
+  "Rakash"
+]
+
+Guilds = [
+  "Barbarians",
+  "Bards",
+  "Clerics",
+  "Empaths",
+  "Moon Mages",
+  "Paladins",
+  "Rangers",
+  "Thieves",
+  "Traders",
+  "Warrior Mages"
+]
