b0ba26f9d6b7e3a04740e79817e5fd5bae0c3cc5
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2009-09-22 20:04:40 -0400

    title

--- app/models/verb.rb
+++ app/models/verb.rb
@@ -2,11 +2,16 @@ class Verb < ActiveRecord::Base
   is_taggable
   
   validates_presence_of :command
+  after_validation :downcase_command
   after_validation :extract_name
   
   cattr_reader :per_page
   @@per_page = 20
 
+  
+  def downcase_command
+    self.command.downcase!
+  end
   
   def extract_name
     self.name = self.command.to_s.match(/\S+/)[0]
